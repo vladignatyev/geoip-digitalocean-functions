5412921b2e6998b183db5919a7455a638f887278
Vladimir Ignatev <ya.na.pochte@gmail.com>
2024-10-03 15:45:13 +0200

    codestyle

--- functions/packages/example/geoIp/__main__.py
+++ functions/packages/example/geoIp/__main__.py
@@ -1,7 +1,5 @@
 from http import HTTPStatus
-
 import geoip2.database
-
 
 UNKNOWN_CONTINENT = 'U'
 UNKNOWN_COUNTRY = 'U'
@@ -13,43 +11,42 @@ ASN_MMDB = 'GeoLite2-ASN.mmdb'
 
 def main(args):
     try:  # Get the IP address of the client from proxy headers
-        ip_addr = args['http'] \
-                      ['headers']['x-forwarded-for']
+        ip_addr = args['http']['headers']['x-forwarded-for']
     except KeyError:
         ip_addr = args.get("ip", None)
-        
+
     if ip_addr is None:
-        return { "statusCode": HTTPStatus.BAD_REQUEST }
-    
+        return {"statusCode": HTTPStatus.BAD_REQUEST}
+
     country_reader = geoip2.database.Reader(COUNTRY_MMDB)
     asn_reader = geoip2.database.Reader(ASN_MMDB)
-    
+
     asn = UNKNOWN_ASN
     continent = UNKNOWN_CONTINENT
     country = UNKNOWN_COUNTRY
-    
+
     try:
-        asn = asn_reader \
-              .asn(ip_addr) \
-                  .autonomous_system_number
+        asn = asn_reader\
+              .asn(ip_addr)\
+              .autonomous_system_number
     except Exception:
         pass
-    
+
     try:
         c = country_reader.country(ip_addr)
         continent = c.continent.code
         country = c.country.iso_code
         is_eu = c.country.is_in_european_union
     except Exception:
-        pass    
-    
+        pass
+
     return {
-        "headers": { 
+        "headers": {
             "Content-Type": "application/json"
         },
-        
+
         "statusCode": HTTPStatus.OK,
-        
+
         "body": {
             "ip": ip_addr,
             "asn": asn,
@@ -57,4 +54,4 @@ def main(args):
             "country": country,
             "is_eu": is_eu
         }
-    } 
+    }
